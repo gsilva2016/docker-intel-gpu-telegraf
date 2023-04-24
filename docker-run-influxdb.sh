613b3516fdcf3fe23275071df4e2107087eb09d3
gsi <gsi@intel.com>
2023-04-24 07:59:04 -0700

    disable db cache

--- docker-run-influxdb.sh
+++ docker-run-influxdb.sh
@@ -9,6 +9,7 @@ docker run --net host -e DOCKER_INFLUXDB_INIT_PASSWORD=p@ssw8rd \
 	-e DOCKER_INFLUXDB_INIT_BUCKET=telegraf-bucket \
 	-e INFLUXD_SESSION_RENEW_DISABLED=true  \
 	-e INFLUXD_SESSION_LENGTH=700 \
-	-v `pwd`/influxdb2-cache:/var/lib/influxdb2 \
 	--name influxdb2 \
 	-itd --rm influxdb:2.6.1
+
+# Bug: unauth: -v `pwd`/influxdb2-cache:/var/lib/influxdb2 \
